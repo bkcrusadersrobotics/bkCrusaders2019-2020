0e7c275906838189c44e832d62c7b93f63c1499b
student <bkcrusadersrobotics@gmail.com>
2019-09-24 15:34:09 -0400

    commit 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intro.java
@@ -57,7 +57,7 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-// this is a test comment
+// this is a test comment hello world
 
 @Autonomous(name="Pushbot: Auto Drive By Time", group="Pushbot")
 @Disabled
